bc919c18d0b6d081b7307caa0fae27fcc146256a
omid-mmd <om.mo1375@gmail.com>
2018-08-09 18:06:34 +0430

    add constructor and destructor

--- secure_prng.hpp
+++ secure_prng.hpp
@@ -1,12 +1,57 @@
+#include <string.h>
+
+#if defined(_WIN32)
+
+#include <stdio.h>
+#include <windows.h>
+#include <wincrypt.h>
+
+#else
+
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#endif
+
 class prng
 {
 public:
   prng(){
+    #if defined(_WIN32)
+      if(!CryptAcquireContext(&prov, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT | CRYPT_SILENT)){
+        std::cerr << "CryptAcquireContext Failed " << std::endl;
+        std::abort;
+      }
+
+    #else
+      if((fd = open("/dev/urandom", O_RDONLY | O_NOCTTY | O_CLOEXEC)) < 0)
+      {
+        std::cerr << "Exit Failure :: open /dev/urandom" << std::endl;
+        std::abort;
+      }
+    #endif    
   }
   
   ~prng(){
+    #if defined(_WIN32)
+      if(!CryptReleaseContext(prov, 0)){
+        std::cerr << "CryptReleaseContext" << std::endl;
+        std::abort;
+      }
+    #else
+      if(close(fd) < 0)
+      {
+        std::cerr << "Exit Failure :: close /dev/urandom " << std::endl; 
+        std::abort;
+      }
+    #endif    
   }
-  
+    
   inline static thread_local prng& inst(){
     static thread_local prng inst;
     return inst;
@@ -16,5 +61,13 @@ public:
     static thread_local const prng& inst = prng::inst();
     return inst;
   }
+  
+  
+private:
+  #if defined(_WIN32)
+    HCRYPTPROV prov;
+  #else
+    int fd;
+  #endif
   
 };
