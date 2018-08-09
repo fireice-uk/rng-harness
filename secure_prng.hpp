538ad7e1bd95652759a5e0e48dea7a5c50fd63f9
omid-mmd <om.mo1375@gmail.com>
2018-08-09 18:13:38 +0430

    fix abort

--- secure_prng.hpp
+++ secure_prng.hpp
@@ -25,14 +25,14 @@ public:
     #if defined(_WIN32)
       if(!CryptAcquireContext(&prov, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT | CRYPT_SILENT)){
         std::cerr << "CryptAcquireContext Failed " << std::endl;
-        std::abort;
+        std::abort();
       }
 
     #else
       if((fd = open("/dev/urandom", O_RDONLY | O_NOCTTY | O_CLOEXEC)) < 0)
       {
         std::cerr << "Exit Failure :: open /dev/urandom" << std::endl;
-        std::abort;
+        std::abort();
       }
     #endif    
   }
@@ -41,13 +41,13 @@ public:
     #if defined(_WIN32)
       if(!CryptReleaseContext(prov, 0)){
         std::cerr << "CryptReleaseContext" << std::endl;
-        std::abort;
+        std::abort();
       }
     #else
       if(close(fd) < 0)
       {
         std::cerr << "Exit Failure :: close /dev/urandom " << std::endl; 
-        std::abort;
+        std::abort();
       }
     #endif    
   }
