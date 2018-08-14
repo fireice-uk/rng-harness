b58af6ce15c3a7de9e67ca872278835301fe586b
CryptoSeyed <cryptoseyed@gmail.com>
2018-08-14 17:03:12 +0430

    generate_random()

--- main.cpp
+++ main.cpp
@@ -4,13 +4,18 @@
 
 
 
+
 int main(int argc, char **argv) 
 {
-    uint8_t sample[64];
+	uint8_t sample[32];
     prng &obj = prng::inst();
-    for(int i = 0; i < 64; i++)
+	obj.generate_random(sample, 32);
+	
+    /*uint8_t sample[64];
+    prng &obj = prng::inst();
+    for(int i = 0; i < 32; i++)
     {
-      obj.generate_system_random_bytes(&sample[i], 1);
+      obj.generate_system_random_bytes(&sample, 1);
     }
         
     for(int i = 0; i < 32; i++)
@@ -19,7 +24,7 @@ int main(int argc, char **argv)
 
     for(int i = 32; i < 64; i++)
       std::cout << std::hex << static_cast<int>(sample[i]);
-    std::cout << std::endl;
+    std::cout << std::endl;*/
     
     /*
     for(int i = 0; i < 32; i++)
--- secure_prng.hpp
+++ secure_prng.hpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include "keccak.c"
 
 #if defined(_WIN32)
 
@@ -21,95 +22,110 @@
 class prng
 {
 public:
-  prng()
-  {
-    #if defined(_WIN32)
-      if(!CryptAcquireContext(&prov, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT | CRYPT_SILENT))
-      {
-        std::cerr << "CryptAcquireContext Failed " << std::endl;
-        std::abort();
-      }
-    #else
-      if((fd = open("/dev/urandom", O_RDONLY | O_NOCTTY | O_CLOEXEC)) < 0)
-      {
-        std::cerr << "Exit Failure :: open /dev/urandom" << std::endl;
-        std::abort();
-      }
-    #endif    
-  }
-  
-  ~prng()
-  {
-    #if defined(_WIN32)
-      if(!CryptReleaseContext(prov, 0))
-      {
-        std::cerr << "CryptReleaseContext" << std::endl;
-        std::abort();
-      }
-    #else
-      if(close(fd) < 0)
-      {
-        std::cerr << "Exit Failure :: close /dev/urandom " << std::endl; 
-        std::abort();
-      }
-    #endif    
-  }
-  
-  inline void generate_system_random_bytes(uint8_t* result, size_t n)
-  {
-    #if defined(_WIN32)  
-      if(CryptGenRandom(prov, (DWORD)n, result))
-      {
-        std::cerr << "CryptGenRandom Failed " << std::endl;
-      }
-    #else
-      while(true)
-      {
-        ssize_t res = read(fd, result, n);
-        if((size_t)res == n)
-        {
-          break;
-        }
-        if(res < 0)
-        {
-          if(errno != EINTR)
-          {
-            std::cerr << "EXIT_FAILURE :: read /dev/urandom" << std::endl;
-            std::abort();
-          }
-        }
-        else if(res == 0)
-        {
-          std::cerr << "EXIT_FAILURE :: read /dev/urandom: end of file " << std::endl;
-          std::abort();
-        }
-        else
-        {
-          result += res;
-          n -= (size_t)res;
-        }
-      }    
-    #endif
-  }
-    
-  inline static thread_local prng& inst()
-  {
-    static thread_local prng inst;
-    return inst;
-  }
-    
-  inline static thread_local const prng& cinst()
-  {
-    static thread_local const prng& inst = prng::inst();
-    return inst;
-  }
-  
-  
+	prng()
+	{
+		#if defined(_WIN32)
+			if(!CryptAcquireContext(&prov, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT | CRYPT_SILENT))
+			{
+				std::cerr << "CryptAcquireContext Failed " << std::endl;
+				std::abort();
+			}
+		#else
+			if((fd = open("/dev/urandom", O_RDONLY | O_NOCTTY | O_CLOEXEC)) < 0)
+			{
+				std::cerr << "Exit Failure :: open /dev/urandom" << std::endl;
+				std::abort();
+			}
+		#endif    
+	}
+
+	~prng()
+	{
+		#if defined(_WIN32)
+			if(!CryptReleaseContext(prov, 0))
+			{
+				std::cerr << "CryptReleaseContext" << std::endl;
+				std::abort();
+			}
+		#else
+			if(close(fd) < 0)
+			{
+				std::cerr << "Exit Failure :: close /dev/urandom " << std::endl; 
+				std::abort();
+			}
+		#endif    
+	}
+
+	void generate_random(uint8_t* result, size_t n, uint8_t* extra=0)
+	{
+		if(n <= 4)
+		{
+			generate_system_random_bytes(result, n);
+			for(int i = 0; i < 4; i++)
+				std::cout << std::hex << static_cast<int>(result[i]);
+			std::cout << std::endl;
+		}
+		else
+		{
+			uint8_t myStaticArray[40];
+
+			// keccak(myStaticArray, 40, , );
+		}
+	}
+
+	inline void generate_system_random_bytes(uint8_t* result, size_t n)
+	{
+		#if defined(_WIN32)  
+			if(CryptGenRandom(prov, (DWORD)n, result))
+			{
+				std::cerr << "CryptGenRandom Failed " << std::endl;
+			}
+		#else
+			while(true)
+			{
+				ssize_t res = read(fd, result, n);
+				if((size_t)res == n)
+				{
+					break;
+				}
+				if(res < 0)
+				{
+					if(errno != EINTR)
+					{
+						std::cerr << "EXIT_FAILURE :: read /dev/urandom" << std::endl;
+						std::abort();
+					}
+				}
+				else if(res == 0)
+				{
+					std::cerr << "EXIT_FAILURE :: read /dev/urandom: end of file " << std::endl;
+					std::abort();
+				}
+				else
+				{
+					result += res;
+					n -= (size_t)res;
+				}
+			}    
+		#endif
+	}
+
+	inline static thread_local prng& inst()
+	{
+		static thread_local prng inst;
+		return inst;
+	}
+
+	inline static thread_local const prng& cinst()
+	{
+		static thread_local const prng& inst = prng::inst();
+		return inst;
+	}
+
 private:
-  #if defined(_WIN32)
-    HCRYPTPROV prov;
-  #else
-    int fd;
-  #endif
-  
+	#if defined(_WIN32)
+		HCRYPTPROV prov;
+	#else
+		int fd;
+	#endif
 };
